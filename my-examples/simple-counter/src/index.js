0b688ca1725651fc9e04ba73374ae7ce3933c6c1
Ismael R <ismael@ismael.desktop>
2017-03-01 23:06:02 +0100

    Avance en el ejemplo counter

--- my-examples/simple-counter/src/index.js
+++ my-examples/simple-counter/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
 import './index.css';
 
 /*
@@ -34,4 +35,72 @@ const SumaContainer = () => {
 ReactDOM.render(
   <SumaContainer />,
   document.getElementById('root')
+);
+
+/*
+  4. Identifica el estado
+    Necesitamos un estado que guarde el contador
+    Sumabutton lanzará un callback para actualizar el contador
+    SumaLable mostrará el contador
+
+    {
+      counter: 0
+    }
+ */
+
+/*
+  5. Definimos las acciones para REDUX
+
+  const COUNTER_ADD = 'COUNTER_ADD'
+  {
+    type: COUNTER_ADD,
+    value: 1
+  }
+
+ */
+
+ const COUNTER_ADD = 'COUNTER_ADD';
+ function counterAdd(value) {
+   return {
+     type: COUNTER_ADD,
+     value
+   }
+ }
+
+// la llamaremos así:  dispatch(counterAdd(value))
+
+
+/*
+  6. Define los reducers
+    Y creamos un store con ellos
+ */
+
+ const initialState = {
+   counter: 0
+ }
+
+ function todoApp(state = initialState, action) {
+   return ({
+     COUNTER_ADD: ()=>{
+       return Object.assign({}, state, {
+         counter: action.value + state.counter
+       })
+     }
+   })[action.type]() || state;
+
+  //  switch (action.type) {
+  //    case COUNTER_ADD:
+  //      return Object.assign({}, state, {
+  //        counter: action.value + state.counter
+  //      })
+  //    default:
+  //      return state
+  //  }
+ }
+
+
+let store = createStore(todoApp);
+
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
 );
